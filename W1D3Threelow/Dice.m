e9ebbeba1366251d9352d359b690b74c2b83d107
Karlo Pagtakhan <kidap@yahoo.com>
2016-03-09 10:39:28 -0500

    Initia project creation

---
+++ W1D3Threelow/Dice.m
@@ -0,0 +1,9 @@
+//
+//  Dice.m
+//  W1D3Threelow
+//
+//  Created by Karlo Pagtakhan on 03/09/2016.
+//  Copyright © 2016 AccessIT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ W1D3Threelow/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  W1D3Threelow
+//
+//  Created by Karlo Pagtakhan on 03/09/2016.
+//  Copyright © 2016 AccessIT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+    return 0;
+}
